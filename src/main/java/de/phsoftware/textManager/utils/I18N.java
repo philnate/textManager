778b9dedfe93c7db55924b1ffae188dcb2252346
philnate <philnate@gmail.com>
2012-05-03 21:57:42 +0200

    converted properties files to UTF8 corresponding changes to I18N to correctly display UTF8

--- src/main/java/de/phsoftware/textManager/utils/I18N.java
+++ src/main/java/de/phsoftware/textManager/utils/I18N.java
@@ -1,12 +1,20 @@
 package de.phsoftware.textManager.utils;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Locale;
 import java.util.MissingResourceException;
+import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
+import java.util.ResourceBundle.Control;
 
 public class I18N {
-    static ResourceBundle captions = ResourceBundle.getBundle("captions",
-	    new Locale("de", "DE"));
+    static ResourceBundle captions = ResourceBundle
+
+    .getBundle("captions", new Locale("de", "DE"), new UTF8Control());
 
     public static String getCaption(String key) {
 	try {
@@ -23,5 +31,48 @@ public class I18N {
 	    caption[i] = getCaption(prefix + "." + suffixes[i]);
 	}
 	return caption;
+    }
+
+    /**
+     * Copied from http://stackoverflow.com/a/4660195
+     * 
+     * @author <a href="http://stackoverflow.com/users/157882/balusc">balusc</a>
+     * 
+     */
+    private static class UTF8Control extends Control {
+	@Override
+	public ResourceBundle newBundle(String baseName, Locale locale,
+		String format, ClassLoader loader, boolean reload)
+		throws IllegalAccessException, InstantiationException,
+		IOException {
+	    // The below is a copy of the default implementation.
+	    String bundleName = toBundleName(baseName, locale);
+	    String resourceName = toResourceName(bundleName, "properties");
+	    ResourceBundle bundle = null;
+	    InputStream stream = null;
+	    if (reload) {
+		URL url = loader.getResource(resourceName);
+		if (url != null) {
+		    URLConnection connection = url.openConnection();
+		    if (connection != null) {
+			connection.setUseCaches(false);
+			stream = connection.getInputStream();
+		    }
+		}
+	    } else {
+		stream = loader.getResourceAsStream(resourceName);
+	    }
+	    if (stream != null) {
+		try {
+		    // Only this line is changed to make it to read properties
+		    // files as UTF-8.
+		    bundle = new PropertyResourceBundle(new InputStreamReader(
+			    stream, "UTF-8"));
+		} finally {
+		    stream.close();
+		}
+	    }
+	    return bundle;
+	}
     }
 }
