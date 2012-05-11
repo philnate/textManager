065323e4ee3a78f61f0ef6c442b84baaae070958
philnate <philnate@gmail.com>
2012-05-11 20:41:36 +0200

    removed development code

--- src/main/java/de/phsoftware/textManager/entities/Setting.java
+++ src/main/java/de/phsoftware/textManager/entities/Setting.java
@@ -2,13 +2,7 @@ package de.phsoftware.textManager.entities;
 
 import static de.phsoftware.textManager.utils.DB.ds;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Iterator;
-
-import javax.swing.JOptionPane;
 
 import com.google.code.morphia.annotations.Entity;
 import com.google.code.morphia.annotations.Id;
@@ -61,47 +55,5 @@ public class Setting {
 	} else {
 	    return new Setting(key, "");
 	}
-    }
-
-    public static void main(String[] args) throws InterruptedException,
-	    IOException {
-	// TODO improve creation of pdf else that it doesn't block or that it's
-	// killed after some time if nothing happened
-	String billId = getBillId();
-	final String texFile = Setting.findSetting("template").getValue()
-		+ billId + ".tex";
-	printOutputStream(new ProcessBuilder("cp", Setting.findSetting(
-		"template").getValue()
-		+ "document.tex", texFile).start());
-	ProcessBuilder pdfLatex = new ProcessBuilder(Setting.findSetting(
-		"pdflatex").getValue(), "-interaction nonstopmode",
-		"-output-format pdf", texFile);
-	pdfLatex.directory(new File(Setting.findSetting("template").getValue()));
-	if (0 == printOutputStream(pdfLatex.start())) {
-	    // display Bill in DocumentViewer
-	    new ProcessBuilder(Setting.findSetting("pdfViewer").getValue(),
-		    texFile.replaceAll("tex$", "pdf")).start().waitFor();
-	} else {
-	    new JOptionPane(
-		    "Bei der Erstellung der Rechnung ist ein Fehler aufgetreten. Es wurde keine Rechnung erstellt.\n Bitte Schauen sie in die Logdatei für nähere Fehlerinformationen.",
-		    JOptionPane.ERROR_MESSAGE).setVisible(true);
-	}
-    }
-
-    private static String getBillId() {
-	return "1";
-    }
-
-    private static int printOutputStream(Process proc) throws IOException,
-	    InterruptedException {
-	InputStream in = new BufferedInputStream(proc.getInputStream());
-	int c;
-	while ((c = in.read()) != -1) {
-	    System.out.print((char) c);
-	}
-	in.close();
-	int rc = proc.waitFor();
-	System.out.println("Return code:" + rc);
-	return rc;
     }
 }
