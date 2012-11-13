5a56f029837e351cca31dc77fadbdaa9822942e9
philnate <philnate@gmail.com>
2012-11-13 20:56:14 +0100

    added license information; fix #34

--- src/main/java/de/phsoftware/textManager/utils/ThreadCompleteListener.java
+++ src/main/java/de/phsoftware/textManager/utils/ThreadCompleteListener.java
@@ -1,3 +1,20 @@
+/**
+ *   textManager, a GUI for managing bills for texter jobs
+ *
+ *   Copyright (C) ${year} philnate
+ *
+ *   This file is part of textManager.
+ *
+ *   textManager is free software: you can redistribute it and/or modify it under the terms of the
+ *   GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ *   License, or (at your option) any later version.
+ *
+ *   textManager is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *   without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *   See the GNU General Public License for more details. You should have received a copy of the GNU
+ *   General Public License along with textManager. If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.phsoftware.textManager.utils;
 
 /**
