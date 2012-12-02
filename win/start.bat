e36132f033b69aeeb5d1049480b64b25e4bb0edd
philnate <philnate@gmail.com>
2012-12-02 21:38:24 +0100

    Missed to rename start.bat to reflect correct jar name

--- win/start.bat
+++ win/start.bat
@@ -8,7 +8,7 @@ echo "in"
 start %p%mongo\mongod.exe -dbpath=%p%\db
 )
 
-java.exe -jar %p%textManager-0.1.0-SNAPSHOT-jar-with-dependencies.jar
+java.exe -jar %p%textManager-1.0.0-SNAPSHOT-jar-with-dependencies.jar
 %p%mongo\mongo.exe admin --eval "db.shutdownServer()"
 
 timeout /T 2
