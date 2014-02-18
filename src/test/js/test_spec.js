f636f1f3977f159b8c31d2f137d1816242109261
Philipp Knobel <pknobel@demandware.com>
2014-02-18 18:37:39 -0500

    Initial travis protractor setup

---
+++ src/test/js/test_spec.js
@@ -0,0 +1,14 @@
+describe('Say Hello', function() {
+  ptor = protractor.getInstance();
+
+  beforeEach(function() {
+    ptor.get('#/');
+    button = ptor.findElement(protractor.By.className('btn-say-hello'));
+    button.click();
+  });
+
+  it('says hello', function() {
+    message = ptor.findElement(protractor.By.className('message'));
+    expect(message.getText()).toEqual('Hello!');
+  });
+});
